bbbd8aa17276158d492d5f062bc001bf0abd71ab
mkausas <mkausas@gmail.com>
2016-02-07 12:13:40 -0500

    Fixed bridging header issues with BDBOAuth1Manager

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,16 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Martynas Kausas on 2/7/16.
+//  Copyright © 2016 Martynas Kausas. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+    
+    
+}
